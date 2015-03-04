0dc5423577bb021fc534a1582584a8e44a675eae
jabriel <jianhuayixiao@gmail.com>
2015-03-04 16:46:32 +0800

    aa
    aa

---
+++ j/youtube.js
@@ -0,0 +1,22 @@
+
+
+//
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '100%',
+        width: '100%',
+        videoId: 'YxnAywSxU1c',
+        playerVars: { 'autoplay': 1 }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
